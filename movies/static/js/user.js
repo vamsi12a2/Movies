a50ecb23e91db1e3eaee2327fc1d3264354c3383
vamsi <vamsi12a2@gmail.com>
2017-10-15 06:39:23 +0000

    new file:   movies/static/css/user.css 	new file:   movies/static/js/user.js 	new file:   users.db

---
+++ movies/static/js/user.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#search").on("keyup", function() {
+        var term = $(this).val().toLowerCase();
+        $("table tbody tr").each(function(){
+            $row = $(this);
+            var name = $row.find("td:nth-child(2)").text().toLowerCase();
+            console.log(name);
+            if(name.search(term) < 0){                
+                $row.hide();
+            } else{
+                $row.show();
+            }
+        });
+    });
+    $('#pagination a').click(function(e){
+        console.log("click "+e)
+    })
+});
